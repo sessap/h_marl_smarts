5bd187c2584ed799e9379f186aaf8a19c723966a
Tucker <montgomery.alban@huawei.com>
2022-02-17 21:10:48 -0500

    Fix dependencies

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "gym==0.19.0",
         "numpy>=1.19.5",
         "pandas>=1.3.4",
-        "psutil>=5.8.0",
+        "psutil>=5.4.8",
         "pybullet==3.0.6",
         "rich>=10.13.0",
         "rtree>=0.9.7",  # Used by sumolib
@@ -43,17 +43,17 @@ setup(
         # The following is for both SS and Envision
         "cloudpickle>=1.3.0,<1.4.0",
         # The following are for /envision
-        "tornado>=6.1",
+        "tornado>=5.1.1",
         "websocket-client>=1.2.1",
         # The following is used for imitation learning and envision
         "ijson>=3.1.4",
         # The following are for the /smarts/algorithms
-        "matplotlib>=3.4.3",
+        "matplotlib>=3.2.2",
         "scikit-image>=0.18.3",
         # The following are for /smarts/zoo and remote agents
         "grpcio==1.32.0",
         "protobuf>=3.19.1",
-        "PyYAML>=6.0",
+        "PyYAML>=3.13",
         "twisted>=21.7.0",
         "opendrive2lanelet>=1.2.1",
     ],
@@ -76,13 +76,15 @@ setup(
         "ros": ["catkin_pkg", "rospkg"],
         "test": [
             # The following are for testing
-            "ipykernel>=6.8.0",
-            "jupyter-client>=7.1.2",
             "pytest>=6.2.5",
             "pytest-benchmark>=3.4.1",
             "pytest-cov>=3.0.0",
-            "pytest-notebook>=0.7.0",
             "pytest-xdist>=2.4.0",
+        ],
+        "test-notebook": [
+            "ipykernel>=4.10.1",
+            "jupyter-client>=7.1.2",
+            "pytest-notebook>=0.7.0",
         ],
         "train": [
             "opencv-python==4.1.2.30",
